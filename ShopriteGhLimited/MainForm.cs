672226ff8dba58ac0f9f3c68cc083065f2382348
ibra <iasafianu@st.ug.edu.gh>
2022-08-21 04:42:46 +0000

    my dcit 318 project

---
+++ ShopriteGhLimited/MainForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopriteGhLimited
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            customizeDesign();
+        }
+
+
+        #region panelSlide
+        private void customizeDesign()
+        {
+            panelSubProduct.Visible = false;
+            panelSubRecord.Visible = false;
+            panelSubStock.Visible = false;
+            panelSubSetting.Visible = false;
+        }
+
+        private void hideSubmenu()
+        {
+            if (panelSubProduct.Visible == true)
+                panelSubProduct.Visible = false;
+            if (panelSubRecord.Visible == true)
+                panelSubRecord.Visible = false;
+            if (panelSubStock.Visible == true)
+                panelSubStock.Visible = false;
+            if (panelSubSetting.Visible == true)
+                panelSubSetting.Visible = false;
+        }
+
+        private void showSubMenu(Panel submenu)
+        {
+            if (submenu.Visible == false)
+            {
+                hideSubmenu();
+                submenu.Visible = true;
+            } else
+                submenu.Visible = false;
+        }
+        #endregion panelSlide
+
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelSubProduct);
+        }
+
+        private void btnProductList_Click(object sender, EventArgs e)
+        {
+            hideSubmenu();
+        }
+        private void btnCategory_Click(object sender, EventArgs e)
+
+        {
+            hideSubmenu();
+        }
+
+        private void btnBrand_Click(object sender, EventArgs e)
+
+        {
+            hideSubmenu();
+        }
+
+        private void btnInStock_Click(object sender, EventArgs e)
+        {
+            hideSubmenu();
+        }
+
+        private void btnStockEntry_Click(object sender, EventArgs e)
+        {
+            hideSubmenu();
+        }
+
+        private void btnStockAdjustment_Click(object sender, EventArgs e)
+        {
+            hideSubmenu();
+        }
+
+        private void btnSupplier_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUser_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
