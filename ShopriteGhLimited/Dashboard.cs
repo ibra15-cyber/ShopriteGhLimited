bfb17afde90e991e7dfd71681a0c0147d9828e55
ibra <iasafianu@st.ug.edu.gh>
2022-08-28 18:40:49 +0000

    dashboard

---
+++ ShopriteGhLimited/Dashboard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopriteGhLimited
+{
+    public partial class Dashboard : Form
+    {
+        SqlConnection cn = new SqlConnection();        
+        DBConnect dbcon = new DBConnect();
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            //string sdate = DateTime.Now.ToShortDateString();
+            string sdate = DateTime.Now.ToString("yyyy-MM-dd");
+            lblDalySale.Text = dbcon.ExtractData("SELECT ISNULL(SUM(total),0) AS total FROM tbCart WHERE status LIKE 'Sold' AND sdate BETWEEN '"+sdate+ "' AND '" + sdate + "'").ToString("#,##0.00");
+            lblTotalProduct.Text = dbcon.ExtractData("SELECT COUNT(*) FROM tbProduct").ToString("#,##0");
+            lblStockOnHand.Text = dbcon.ExtractData("SELECT ISNULL(SUM(qty), 0) AS qty FROM tbProduct").ToString("#,##0");
+            lblCriticalItems.Text = dbcon.ExtractData("SELECT COUNT(*) FROM vwCriticalItems").ToString("#,##0"); 
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
